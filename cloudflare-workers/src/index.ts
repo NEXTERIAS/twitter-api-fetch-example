1258b6ad398c33b1f5f8815687da406817882d32
InkoHX <me@inkohx.dev>
2023-01-21 21:00:39 +0900

    :art: format code

--- cloudflare-workers/src/index.ts
+++ cloudflare-workers/src/index.ts
@@ -1,31 +1,31 @@
 import { oauth1a } from 'twitter-api-fetch/edge'
 
 export interface Env {
-	TWITTER_API_ACCESS_TOKEN: string
-	TWITTER_API_SECRET_ACCESS_TOKEN: string
-	TWITTER_API_CONSUMER_KEY: string
-	TWITTER_API_SECRET_CONSUMER_KEY: string
+  TWITTER_API_ACCESS_TOKEN: string
+  TWITTER_API_SECRET_ACCESS_TOKEN: string
+  TWITTER_API_CONSUMER_KEY: string
+  TWITTER_API_SECRET_CONSUMER_KEY: string
 }
 
 export default {
-	async fetch(
-		_request: Request,
-		env: Env,
-		_ctx: ExecutionContext
-	): Promise<Response> {
-		const fetcher = await oauth1a({
-			accessToken: env.TWITTER_API_ACCESS_TOKEN,
-			consumerKey: env.TWITTER_API_CONSUMER_KEY,
-			secretAccessToken: env.TWITTER_API_SECRET_ACCESS_TOKEN,
-			secretConsumerKey: env.TWITTER_API_SECRET_CONSUMER_KEY
-		})
-		const response = await fetcher(
+  async fetch(
+    _request: Request,
+    env: Env,
+    _ctx: ExecutionContext
+  ): Promise<Response> {
+    const fetcher = await oauth1a({
+      accessToken: env.TWITTER_API_ACCESS_TOKEN,
+      consumerKey: env.TWITTER_API_CONSUMER_KEY,
+      secretAccessToken: env.TWITTER_API_SECRET_ACCESS_TOKEN,
+      secretConsumerKey: env.TWITTER_API_SECRET_CONSUMER_KEY
+    })
+    const response = await fetcher(
       "/2/users/910317474951786496/tweets",
-			{
-				cf: {
-					cacheTtl: 600
-				}
-			}
+      {
+        cf: {
+          cacheTtl: 600
+        }
+      }
     );
 
     if (!response.ok) {
@@ -37,5 +37,5 @@ export default {
     console.log(tweets);
 
     return new Response(JSON.stringify(tweets));
-	},
+  },
 };
